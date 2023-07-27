7394a3c70580ffdeca1a29e34381f1bc06adb319
Likhitha <nlikhitha63@gmail.com>
2023-07-27 18:07:07 +0530

    Day 26: Add invoice date ,row,column

--- main.py
+++ main.py
@@ -29,9 +29,9 @@ for filepath in filepaths:
     pdf.set_text_color(80, 80, 80)
     pdf.cell(w=30, h=8, txt=columns[0], border=1)
     pdf.cell(w=70, h=8, txt=columns[1], border=1)
-    pdf.cell(w=30, h=8, txt=columns[2], border=1)
+    pdf.cell(w=37, h=8, txt=columns[2], border=1)
     pdf.cell(w=30, h=8, txt=columns[3], border=1)
-    pdf.cell(w=30, h=8, txt=columns[4], border=1, ln=1)
+    pdf.cell(w=25, h=8, txt=columns[4], border=1, ln=1)
 
     # Add rows to the table
     for index, row in df.iterrows():
@@ -39,8 +39,8 @@ for filepath in filepaths:
         pdf.set_text_color(80, 80, 80)
         pdf.cell(w=30, h=8, txt=str(row["product_id"]), border=1)
         pdf.cell(w=70, h=8, txt=str(row["product_name"]), border=1)
-        pdf.cell(w=30, h=8, txt=str(row["amount_purchased"]), border=1)
+        pdf.cell(w=37, h=8, txt=str(row["amount_purchased"]), border=1)
         pdf.cell(w=30, h=8, txt=str(row["price_per_unit"]), border=1)
-        pdf.cell(w=30, h=8, txt=str(row["total_price"]), border=1, ln=1)
+        pdf.cell(w=25, h=8, txt=str(row["total_price"]), border=1, ln=1)
 
     pdf.output(f"PDFs/{filename}.pdf")
